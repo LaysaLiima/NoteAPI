b5f0f21dcaf6e3363e86c43034ab2f10a7c3c002
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-08-15 10:50:11 -0300

    Feat: Adiciona arquivo rotas.js

---
+++ src/rotas.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const notes = require('./Controllers/NoteController.js');
+
+const rotas = express();
+
+rotas.get('/notes', notes.listarAnotacoes); 
+rotas.get('/notes/:id', notes.obterUmaNota); 
+rotas.post('/notes', notes.cadastrarNota); 
+rotas.put('/notes/:id', notes.atualizarUmaNota); 
+rotas.delete('/notes/:id', notes.deletarUmaNota); 
+
+module.exports = rotas;
