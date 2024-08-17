58dfc3432fbc95425dbe8b993b52f67495f8eee1
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-08-17 09:26:21 -0300

    feat: adicionar rota para obter localização por IP

--- src/rotas.js
+++ src/rotas.js
@@ -3,10 +3,11 @@ const notes = require('./Controllers/NoteController.js');
 
 const rotas = express();
 
+rotas.get('/notes/location', notes.obterLocalizacaoPorIp); 
 rotas.get('/notes', notes.listarAnotacoes); 
 rotas.get('/notes/:id', notes.obterUmaNota); 
 rotas.post('/notes', notes.cadastrarNota); 
 rotas.put('/notes/:id', notes.atualizarUmaNota); 
-rotas.delete('/notes/:id', notes.deletarUmaNota); 
+rotas.delete('/notes/:id', notes.deletarUmaNota);
 
 module.exports = rotas;
