e1d354973397e639ba4cb28c0d1b38bfad41c8f9
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-08-19 11:52:26 -0300

    feat: adiciona campo de data de criação no schema de notas

--- src/Models/Note.js
+++ src/Models/Note.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const notaSchema = new mongoose.Schema({
     titulo: { type: String, required: true },
-    conteudo: { type: String, required: true }
+    conteudo: { type: String, required: true },
+    datCriacao: {type: Date, default: Date.now}
 });
 
 const Nota = mongoose.model('Nota', notaSchema);
